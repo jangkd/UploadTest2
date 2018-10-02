f9e3c1dba8cefe7c91ebaf17df97bfd755939ba5
jangkd <tel94949114@gmail.com>
2018-10-02 10:20:49 +0900

    다시 교실에서 수정함.. 주석라인 삭제

--- SpringFirst/src/first/common/service/CommonService.java
+++ SpringFirst/src/first/common/service/CommonService.java
@@ -3,7 +3,5 @@ package first.common.service;
 import java.util.Map;
 
 public interface CommonService {
-// 주석 라인추가 
 	Map<String, Object> selectFileInfo(Map<String, Object> map) throws Exception;
-
 }
